c8429093cef6f75057fd842777c608ba19ac5829
Thanatos0173 <thanatos0173@gmail.com>
2025-02-02 23:01:44 +0100

    Modified documentation for start

--- sort.h
+++ sort.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 
 /**
- * @brief Recursive implementation of a quicksort. The array is sorted in place.
+ * @brief
+ * Recursive implementation of a quicksort. The array is sorted in place.
  * @param list An array to sort.
  * @param *isGreater The pointer to a function that, given two elements, return true if the first element is strictly greater than the second.
  * @param elem_size The size of an element in the array.
